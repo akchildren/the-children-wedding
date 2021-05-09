084d0aa55598e471be1bf169390392eda71a96d8
Adam Children <adam.children@themix.org.uk>
2021-05-10 00:38:38 +0100

    Adam: Init boilerplate for site

--- myapp/resources/views/layouts/app.blade.php
+++ myapp/resources/views/layouts/app.blade.php
@@ -1,19 +1,32 @@
 <!-- resources/views/components/layout.blade.php -->
 
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Website for Adam's and Rachel's wedding <3" />
+    <meta name="author" content="Adam Children" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
     <title>{{"Adam's & Rachel's Wedding" }}</title>
 </head>
-<body>
-@section('sidebar')
-    This is the master sidebar.
-@show
+<body class="page-top">
+{{--@section('sidebar')--}}
+{{--    This is the master sidebar.--}}
+{{--@show--}}
 
-<div class="container mx-auto">
+{{--<div class="container">--}}
     @yield('content')
-</div>
+{{--</div>--}}
+<!-- Bootstrap core JS-->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Third party plugin JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
+<script href="{{ asset('js/scripts.js') }}" rel="stylesheet"></script>
+
 </body>
 </html>
