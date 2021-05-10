fa16e90e8588e1e70e2b4e424320b3e622866a56
Adam Children <adam.children@themix.org.uk>
2021-05-10 16:29:31 +0100

    Adam: add more mvps + favicon

--- myapp/resources/views/components/mvps.blade.php
+++ myapp/resources/views/components/mvps.blade.php
@@ -2,11 +2,11 @@
 <section class="page-section bg-light" id="team">
     <div class="container">
         <div class="text-center">
-            <h2 class="section-heading text-uppercase">Groomsmen & Bridesmaids</h2>
+            <h2 class="section-heading text-uppercase">Groomsmen</h2>
             <h3 class="section-subheading text-muted">Our saviours</h3>
         </div>
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-lg-3">
                 <div class="team-member">
                     <img class="mx-auto rounded-circle" src="{{asset('img/Danny.jpg')}}" alt="..." />
                     <h4>Danny Tull</h4>
@@ -16,17 +16,37 @@
                     <a class="btn btn-dark btn-social mx-2" href="https://api.whatsapp.com/send?phone=+447852346972"><i class="fa fa-whatsapp"></i></a>
                 </div>
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-3">
                 <div class="team-member">
                     <img class="mx-auto rounded-circle" src="{{asset('img/liam.jpeg')}}" alt="..." />
                     <h4>Liam Askew</h4>
                     <p class="text-muted">Best Man 2</p>
-                    <a class="btn btn-dark btn-social mx-2" href="mailto:danny@dtull.co.uk?subject=Adam%20and%20Rachel%27s%20Wedding"><i class="fa fa-envelope"></i></a>
+                    <a class="btn btn-dark btn-social mx-2" href="mailto:liamaskew@yahoo.co.uk?subject=Adam%20and%20Rachel%27s%20Wedding"><i class="fa fa-envelope"></i></a>
                     <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/liam.askew1"><i class="fa fa-facebook"></i></a>
                     <a class="btn btn-dark btn-social mx-2" href="https://api.whatsapp.com/send?phone=+447393194288"><i class="fa fa-whatsapp"></i></a>
                 </div>
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-3">
+                <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="{{asset('img/unknown.png')}}" alt="..." />
+                    <h4>TBA</h4>
+                    <p class="text-muted">Groomsman</p>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="{{asset('img/unknown.png')}}" alt="..." />
+                    <h4>TBA</h4>
+                    <p class="text-muted">Groomsman</p>
+                </div>
+            </div>
+        </div>
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">Bridesmaids</h2>
+            <h3 class="section-subheading text-muted">Our saviours</h3>
+        </div>
+        <div class="row">
+            <div class="col-lg-3">
                 <div class="team-member">
                     <img class="mx-auto rounded-circle" src="{{asset('img/Ruth.jpg')}}" alt="..." />
                     <h4>Ruth Geraghty</h4>
@@ -34,6 +54,27 @@
                     <a class="btn btn-dark btn-social mx-2" href="mailto:danny@dtull.co.uk?subject=Adam%20and%20Rachel%27s%20Wedding"><i class="fa fa-envelope"></i></a>
                     <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/ruth.geraghty.7"><i class="fa fa-facebook"></i></a>
                     <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fa fa-whatsapp"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="{{asset('img/sarah.jpeg')}}" alt="..." />
+                    <h4>Sarah Williams</h4>
+                    <p class="text-muted">Bridesmaid</p>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="{{asset('img/Holly.jpg')}}" alt="..." />
+                    <h4>Holly Milner</h4>
+                    <p class="text-muted">Bridesmaid</p>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="{{asset('img/Lucy.jpg')}}" alt="..." />
+                    <h4>Lucy Children</h4>
+                    <p class="text-muted">Bridesmaid</p>
                 </div>
             </div>
         </div>
--- myapp/resources/views/layouts/app.blade.php
+++ myapp/resources/views/layouts/app.blade.php
@@ -6,22 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="Website for Adam's and Rachel's wedding <3" />
     <meta name="author" content="Adam Children" />
+    <link rel="icon"
+          type="image/png"
+          href="{{asset('img/logo-removebg-preview-crop.png')}}" />
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
     <script src="https://use.fontawesome.com/3f9e0b1a49.js"></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
     <title>{{"Adam's & Rachel's Wedding" }}</title>
 </head>
 <body class="page-top">
-{{--@section('sidebar')--}}
-{{--    This is the master sidebar.--}}
-{{--@show--}}
 
-{{--<div class="container">--}}
     @yield('content')
-{{--</div>--}}
+
 <!-- Bootstrap core JS-->
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
