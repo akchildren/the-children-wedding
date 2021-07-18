b7acc6243590cee318699bad52b4a717d751abc4
Adam Children <adam.children@themix.org.uk>
2021-07-18 16:11:47 +0100

    Adam: Add history of bradbourne

--- myapp/resources/views/components/partials/nav-list.blade.php
+++ myapp/resources/views/components/partials/nav-list.blade.php
@@ -1,9 +1,11 @@
 <ul class="navbar-nav text-uppercase ml-auto">
 {{--    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a></li>--}}
     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#team">Wedding Roles</a></li>
+    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#playlist">Playlist</a></li>
+    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#gifts">Gifts</a></li>
     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#location">Location</a></li>
     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
-    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Food Menu</a></li>
-    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Venue History</a></li>
+    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#">Food Menu</a></li>
+    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{asset("pdf/History-of-Bradbourne.pdf")}}" target="_blank">Venue History</a></li>
     <li class="nav-item"><a class="nav-link js-scroll-trigger RSVP" href="#rsvp">RSVP</a></li>
 </ul>
