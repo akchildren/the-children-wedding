cef84f5d993dcb09dcfe7e82bc921ace60d7477c
Adam Children <adam.children@themix.org.uk>
2021-07-22 22:55:13 +0100

    Adam: add no index to all crawlers

--- myapp/resources/views/layouts/app.blade.php
+++ myapp/resources/views/layouts/app.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="Website for Adam's and Rachel's wedding <3" />
     <meta name="author" content="Adam Children" />
-    <meta name="robots" content="noindex">
+    <meta name="robots" content="none, notranslate, nosnippet">
+    <meta name="googlebot" content="none, notranslate, nosnippet">
+    <meta name="googlebot-news" content="none, notranslate, nosnippet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
