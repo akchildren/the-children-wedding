71122d3df4e96840a70e7a05aaffbf47202842d0
Adam Children <adam.children@themix.org.uk>
2021-07-22 22:51:24 +0100

    Adam: Remove unused icon

--- myapp/resources/views/layouts/app.blade.php
+++ myapp/resources/views/layouts/app.blade.php
@@ -7,10 +7,6 @@
     <meta name="description" content="Website for Adam's and Rachel's wedding <3" />
     <meta name="author" content="Adam Children" />
     <meta name="robots" content="noindex">
-    <link rel="icon"
-          type="image/png"
-          href="{{asset('img/logo-removebg-preview-crop.png')}}" />
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
