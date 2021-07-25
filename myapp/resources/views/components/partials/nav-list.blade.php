08d5404bb9defe647ecfc487c581f844bad56911
Adam Children <adam.children@themix.org.uk>
2021-07-25 15:14:35 +0100

    Adam: Comment out menu navigation option

--- myapp/resources/views/components/partials/nav-list.blade.php
+++ myapp/resources/views/components/partials/nav-list.blade.php
@@ -5,7 +5,7 @@
     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#gifts">Gifts</a></li>
     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#location">Location</a></li>
 {{--    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>--}}
-    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#">Food Menu</a></li>
-    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{asset("pdf/History-of-Bradbourne.pdf")}}" target="_blank">Venue History</a></li>
+{{--    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#">Food Menu</a></li>--}}
+    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{asset("public/pdf/History-of-Bradbourne.pdf")}}" target="_blank">Venue History</a></li>
     <li class="nav-item"><a class="nav-link js-scroll-trigger RSVP" href="#rsvp">RSVP</a></li>
 </ul>
