f2a074644a53a117012574abb7c869b56f896260
Adam Children <adam.children@themix.org.uk>
2022-02-06 18:35:57 +0000

    Adam: Update composer lock + Hotel location to orida

--- myapp/resources/views/components/hotel.blade.php
+++ myapp/resources/views/components/hotel.blade.php
@@ -2,19 +2,20 @@
     <div class="container">
         <div class="row text-center">
             <h2 class="section-heading text-uppercase">Accommodation</h2>
-            <p class="large text-muted">We have arranged a bulk deal with the Hilton Hotel to provide discounted rooms for the wedding day.<br>
-                Please select the image below to find the best room for you.</p>
-            <p class="large text-muted"><strong style="color: #ee2958">We have temporarily removed the hotel link provided by the Hilton as the hotel is under going ownership change.<br>
-                We will keep this website updated with a new link once it's available :)</strong></p>
-            <p class="medium text-muted"><strong style="color: #e76d8b">If you have already booked a room at the Hilton, you would have received an email and a full refund.</strong></p>
-{{--                <a href="http://eventsathilton.com/show/614afbeb37069e683859a5f2"></a>--}}
-{{--            <div class="col-md-12">--}}
-{{--                <div style="width:200px;margin:0 auto;">--}}
-{{--                    <a href="http://eventsathilton.com/show/614afbeb37069e683859a5f2">--}}
-{{--                        <img src="{{asset(env('ASSET_LOCATION').'/img/logos/hilton.png')}}" style="width: 100%">--}}
-{{--                    </a>--}}
-{{--                </div>--}}
-{{--            </div>--}}
+            <p class="large text-muted">We have arranged a bulk deal with the Orida Hotels to provide discounted rooms on the wedding day.<br>
+                To book your room, use the phone number provided to call them and say you are booking under the reference of <strong>"Rachel Franklin".</strong></p>
+            <h3>Phone Number: <a href="tel:01622 734 322" class="Blondie">01622 734 322</a></h3>
+            <div class="col-md-12 mt-3">
+                <div style="width:200px;margin:0 auto;">
+                    <a href="http://eventsathilton.com/show/614afbeb37069e683859a5f2">
+                        <img src="https://www.oridahotels.com/wp-content/uploads/2021/11/orida-hotel-top-logo.svg" style="width: 100%">
+                    </a>
+                </div>
+            </div>
+            <p class="medium text-muted mt-3"><strong style="color: #e76d8b;">If you have already booked a room at the Hilton, you would have received an email and a full refund.
+                <br>If you did book using hitlon, you can call this number up and rebook the same room.
+                </strong></p>
+
         </div>
     </div>
 </section>
